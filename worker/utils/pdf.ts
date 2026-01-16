f8383a742cdb881860a0abb95c0b732f152102f2
Wayne Summers <waynesummers12@gmail.com>
2026-01-16 11:56:55 -0700

    commit

--- worker/utils/pdf.ts
+++ worker/utils/pdf.ts
@@ -1,19 +1,22 @@
 // worker/utils/pdf.ts
-import pdfjs from "https://esm.sh/pdfjs-dist@4.0.379";
+import * as pdfjs from "https://esm.sh/pdfjs-dist@4.0.379/legacy/build/pdf.mjs";
 
-export async function extractTextFromPdf(bytes: Uint8Array): Promise<string> {
-  const pdf = await pdfjs.getDocument({ data: bytes }).promise;
+export async function extractTextFromPdf(pdfBytes: Uint8Array): Promise<string> {
+  const loadingTask = pdfjs.getDocument({ data: pdfBytes });
+  const pdf = await loadingTask.promise;
 
-  let fullText = "";
+  let text = "";
 
   for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     const content = await page.getTextContent();
-    const pageText = content.items
-      .map((item: any) => item.str)
-      .join(" ");
-    fullText += `\n${pageText}`;
+
+    for (const item of content.items as any[]) {
+      if (item.str) {
+        text += item.str + " ";
+      }
+    }
   }
 
-  return fullText;
+  return text;
 }
