5d219027c6e7236eb5db269ce87aeb1a390ede02
Wayne Summers <waynesummers12@gmail.com>
2026-01-16 11:29:11 -0700

    commit

---
+++ web/src/worker/utils/storage.ts
@@ -0,0 +1,17 @@
+{
+  "compilerOptions": {
+    "lib": ["deno.ns", "deno.window", "esnext"]
+  },
+  "lint": {
+    "rules": {
+      "tags": ["recommended"]
+    }
+  },
+  "fmt": {
+    "options": {
+      "useTabs": false,
+      "lineWidth": 100,
+      "indentWidth": 2
+    }
+  }
+}
