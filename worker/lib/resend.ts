f7667da13b11a6ee172856e58f85b7ace0ae50f0
Wayne Summers <waynesummers12@gmail.com>
2026-01-20 16:36:22 -0700

    commit

--- worker/lib/resend.ts
+++ worker/lib/resend.ts
@@ -1,6 +1,12 @@
 // worker/lib/resend.ts
+
 import { Resend } from "npm:resend@3.5.0";
 
-export const resend = new Resend(
-  Deno.env.get("RESEND_API_KEY")!
-);
+const apiKey = Deno.env.get("RESEND_API_KEY");
+
+if (!apiKey) {
+  throw new Error("RESEND_API_KEY is not set");
+}
+
+export const resend = new Resend(apiKey);
+
