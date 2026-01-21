4916a21015393263447d66dbcacc86d939768c13
Wayne Summers <waynesummers12@gmail.com>
2026-01-20 18:22:35 -0700

    commit

--- worker/main.ts
+++ worker/main.ts
@@ -7,6 +7,7 @@ import auditPdfRoutes from "./routes/auditPdf.ts";
 import latestAuditRoutes from "./routes/latestAudit.ts";
 import auditsRoutes from "./routes/audits.ts";
 import stripeWebhookRoutes from "./routes/stripeWebhook.ts";
+import auditByIdRoutes from "./routes/auditById.ts";
 
 const app = new Application();
 const router = new Router();
@@ -78,6 +79,9 @@ app.use(checkoutRoutes.allowedMethods());
 
 app.use(auditPdfRoutes.routes());
 app.use(auditPdfRoutes.allowedMethods());
+
+router.use(auditByIdRoutes.routes());
+
 
 /* -------------------- LISTEN -------------------- */
 const PORT = Number(Deno.env.get("PORT") ?? 8000);
