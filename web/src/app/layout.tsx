777e985d4f72b6f8a8429045eba94a17705f8c00
Wayne Summers <waynesummers12@gmail.com>
2026-01-21 15:47:43 -0700

    commit

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col bg-white text-gray-900">
         <Header />
 
-        <main className="flex-1 pt-28">
+        <main className="flex-1 pt-24 md:pt-28">
   {children}
 </main>
 
