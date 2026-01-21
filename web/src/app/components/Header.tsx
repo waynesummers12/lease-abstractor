3d0c62bd8328949d2f9d6cc03ec87058d06e7149
Wayne Summers <waynesummers12@gmail.com>
2026-01-20 22:07:15 -0700

    commit

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import DashboardNav from "@/components/dashboard/DashboardNav";
 
 export default function Header() {
   const pathname = usePathname();
