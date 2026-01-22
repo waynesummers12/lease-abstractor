b40d3bd9d473cc5b92238e378587039abd8a5d52
Wayne Summers <waynesummers12@gmail.com>
2026-01-22 08:30:58 -0700

    commit layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 // web/src/app/layout.tsx
 
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function RootLayout({
   children,
