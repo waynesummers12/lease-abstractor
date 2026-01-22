6d9f6ba32db36c2c7ae5aa4c33371f63c12a29a2
Wayne Summers <waynesummers12@gmail.com>
2026-01-21 17:00:03 -0700

    commit

--- web/src/app/marketing/page.tsx
+++ web/src/app/marketing/page.tsx
@@ -1,6 +1,4 @@
 // src/app/marketing/page.tsx
-import Link from "next/link";
-
 export default function MarketingIndexPage() {
   return (
     <main className="mx-auto max-w-3xl px-6 py-20 text-center">
@@ -12,19 +10,6 @@ export default function MarketingIndexPage() {
         Learn how SaveOnLease helps commercial tenants uncover CAM and NNN
         overcharges.
       </p>
-
-      <div className="flex justify-center gap-4">
-        <Link href="/marketing/how-it-works" className="underline">
-          How it Works
-        </Link>
-        <Link href="/marketing/what-we-find" className="underline">
-          What We Find
-        </Link>
-        <Link href="/marketing/pricing" className="underline">
-          Pricing
-        </Link>
-      </div>
     </main>
   );
 }
-
