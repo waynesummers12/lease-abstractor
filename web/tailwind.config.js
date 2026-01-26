bc0d9dbc2d13966175f6928daf46b4e2f19378eb
Wayne Summers <waynesummers12@gmail.com>
2026-01-25 21:29:58 -0700

    comit

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
