2383904e8cb0da808c8bef9de07a4c71d07ba80e
Wayne Summers <waynesummers12@gmail.com>
2026-01-28 08:38:10 -0700

    commit

--- web/src/app/app/step-3-review/page.tsx
+++ web/src/app/app/step-3-review/page.tsx
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic";
 
-import Step3ReviewClient from "./Step3ReviewClient";
+import Step3ReviewClient from "./step3ReviewClient";
 
 export default function Step3ReviewPage() {
   return <Step3ReviewClient />;
