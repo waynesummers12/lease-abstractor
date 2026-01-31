6fa99716329c2ca2c37515b5e625ea6bbd0b8ea1
Wayne Summers <waynesummers12@gmail.com>
2026-01-31 14:37:36 -0700

    commit

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/lib/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./src/app/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
     extend: {},
   },
