2151c749315308dde3df0b1b3e80c24c7330a548
Wayne Summers <waynesummers12@gmail.com>
2026-02-13 09:32:56 -0700

    commit

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -1,13 +1,25 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://saveonlease.com";
+
   return {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: [
+          "/",
+          "/marketing/",
+          "/app",
+        ],
+        disallow: [
+          "/api/",
+          "/_next/",
+          "/dashboard/",
+          "/private/",
+        ],
       },
     ],
-    sitemap: "https://saveonlease.com/sitemap.xml",
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
