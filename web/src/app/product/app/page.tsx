67902532c669a45ee8ae66ab3a8eb66b1a1b5335
Wayne Summers <waynesummers12@gmail.com>
2026-02-17 12:30:38 -0700

    commit

--- web/src/app/product/app/page.tsx
+++ web/src/app/product/app/page.tsx
@@ -1,1 +1,5 @@
-export { default } from "../../app/step-3-review/page";
+import { redirect } from "next/navigation";
+
+export default function ProductAppRedirect() {
+  redirect("/app/step-1-upload");
+}
