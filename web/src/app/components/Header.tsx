b9e425b6928049b1973d32f3ba3353bc66dea1b4
Wayne Summers <waynesummers12@gmail.com>
2026-03-11 16:13:26 -0600

    commit

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -376,7 +376,12 @@ export default function Header() {
           >
             How It Works
           </Link>
-
+          <Link
+            href="/marketing/referral"
+             className={`relative ${isAppPage ? "text-gray-900" : "text-white"} opacity-80 hover:opacity-100 after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full`}
+            >
+            Refer Clients & Earn
+          </Link>
           <Link
             href="/marketing/pricing"
             className={`relative ${isAppPage ? "text-gray-900" : "text-white"} opacity-80 hover:opacity-100 after:absolute after:-bottom-1 after:left-0 after:h-[2px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full`}
@@ -556,6 +561,9 @@ export default function Header() {
       <Link href="/marketing/how-it-works" onClick={() => setMenuOpen(false)}>
         How It Works
       </Link>
+      <Link href="/marketing/referral" onClick={() => setMenuOpen(false)}>
+       Refer Clients & Earn
+       </Link>
       <Link href="/marketing/pricing" onClick={() => setMenuOpen(false)}>
         Pricing
       </Link>
