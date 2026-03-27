e2649834aa78e3a1a7541fceb1d041c7211c870e
Wayne Summers <waynesummers12@gmail.com>
2026-03-27 15:53:31 -0600

    commit

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -78,7 +78,7 @@ export default function Header() {
           <Link href="/marketing/how-it-works" className="opacity-70 hover:opacity-100 transition">
             How It Works
           </Link>
-          <Link href="/marketing/referral" className="opacity-70 hover:opacity-100 transition">
+          <Link href="/marketing/referral" className="opacity-90 hover:opacity-100 font-medium transition">
             Refer Clients (Earn 20%)
           </Link>
           <Link href="/marketing/contact" className="opacity-70 hover:opacity-100 transition">
