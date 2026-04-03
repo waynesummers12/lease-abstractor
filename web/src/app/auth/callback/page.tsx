c64c2d9d80a39dfa5be09a72d4024c0dd6e1698a
Wayne Summers <waynesummers12@gmail.com>
2026-04-02 20:23:52 -0600

    commit

--- web/src/app/auth/callback/page.tsx
+++ web/src/app/auth/callback/page.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { createBrowserClient } from "@supabase/ssr";
 import { useRouter, useSearchParams } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function AuthCallback() {
   const router = useRouter();
   const searchParams = useSearchParams();
