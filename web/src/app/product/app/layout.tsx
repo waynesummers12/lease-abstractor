9d1e778a1df0845c80ebdae694f3c0e6152dfa1e
Wayne Summers <waynesummers12@gmail.com>
2026-04-03 10:19:26 -0600

    commit

--- web/src/app/product/app/layout.tsx
+++ web/src/app/product/app/layout.tsx
@@ -11,7 +11,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@/app/components/ui/dropdown-menu";
 
 export function AvatarDropdown() {
 
